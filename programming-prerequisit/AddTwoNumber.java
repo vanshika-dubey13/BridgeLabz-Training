2396fd9a92d13714e9bb998272d28702dd1b7642
Vanshika Dubey <dubeyvanshika021@gmail.com>
2025-12-17 08:47:54 +0530

    [Vanshika] : added AddTwoNumbers core java program

---
+++ programming-prerequisit/AddTwoNumber.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class AddTwoNumber{
+	public static void main(String[] args){
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Enter first number :");
+	int num1 = sc.nextInt();
+	System.out.println("Enter second number :");
+	int num2 = sc.nextInt();
+	int ans = num1 + num2;
+	System.out.println(ans);
+	}
+}
+	
