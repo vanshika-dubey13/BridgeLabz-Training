bef1f5139dfe410c4833786a56c030546249cc94
Vanshika Dubey <dubeyvanshika021@gmail.com>
2025-12-17 12:09:14 +0530

    [Vanshika] : added AgeOfHarry core java program assignment

---
+++ gcr-codebase/programming-elements/Level-1/AgeOfHarry.java
@@ -0,0 +1,17 @@
+//Creating class AgeOfHarry to calculate current age of Harry
+
+public class AgeOfHarry{
+	public static void main(String[] args){
+	//Birth year of Harry
+	int birthyear = 2000;
+
+	//Cuurent year is
+	int currentYear = 2024;
+
+	//Calculating current age of Harry
+	int currentAge = currentYear - birthyear;
+
+	//Displaying currentAge of Harry
+	System.out.println("Harry is "+ currentAge + " years old in " + currentYear);
+	}
+}
