259c7f7afc3302a1a0ec6544ccbd8a527b6fb6bc
Vanshika Dubey <dubeyvanshika021@gmail.com>
2025-12-17 09:03:31 +0530

    [Vanshika] : added RectanglePerimeter core java program

---
+++ programming-prerequisit/RectanglePerimeter.java
@@ -0,0 +1,12 @@
+public class RectanglePerimeter{
+	public static void main(String [] args){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter length of rectangle :");
+		int length = sc.nextInt();
+		System.out.println("Enter width of rectangle :");
+		int width = sc.nextInt();
+		int peri = 2 * (length + breadth);
+		System.out.println(peri);
+
+	}
+}
