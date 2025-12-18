2a6ddeb8965d35e1878cae8c3915c457cf843e46
Vanshika Dubey <dubeyvanshika021@gmail.com>
2025-12-18 18:27:55 +0530

    [Vanshika] : added LargestOfThree core java assignment

--- gcr-codebase/control-flows/Level-1/LargestOfThree.java
+++ gcr-codebase/control-flows/Level-1/LargestOfThree.java
@@ -1,26 +1,29 @@
-//Creating a class to print largest of 3 numbers
+//Creating a class to find largest of 3 numbers
 
 import java.util.Scanner;
-public class LargestOfThree{
-	public static void main(String [] args){
-	Scanner sc = new Scanner(System.in);
-	//Taking 3 numbers from user
-	System.out.print("Enter 3 numbers : ");
-	int num1 = sc.nextInt();
-	int num2 = sc.nextInt();
-	int num3 = sc.nextInt();
 
-	if( (num1 > num2) && (num1 > num3) ){
-		System.out.print("Number 1 is largest");
-		}
-	else if( (num2 > num1) && (num2 > num3) ){
-		System.out.print("Number 2 is largest");
-		}
-	else{
-		System.out.print("Number 3 is largest");
+public class LargestOfThree {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-		}
-sc.close();
+        // Input three numbers
+        System.out.print("Enter the first number: ");
+        int num1 = sc.nextInt();
+        System.out.print("Enter the second number: ");
+        int num2 = sc.nextInt();
+        System.out.print("Enter the third number: ");
+        int num3 = sc.nextInt();
 
-	}
+        // Checking  which number is the largest
+        boolean isFirstLargest = (num1 >= num2) && (num1 >= num3);
+        boolean isSecondLargest = (num2 >= number1) && (num2 >= num3);
+        boolean isThirdLargest = (num3 >= num1) && (num3 >= num2);
+
+        // Output results
+        System.out.println("Is the first number the largest? " + isFirstLargest);
+        System.out.println("Is the second number the largest? " + isSecondLargest);
+        System.out.println("Is the third number the largest? " + isThirdLargest);
+
+        sc.close();
+    }
 }
