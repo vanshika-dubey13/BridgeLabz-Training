1110799892554ed2ff1b7ac0cbff29bdd81b8681
Vanshika Dubey <dubeyvanshika021@gmail.com>
2025-12-19 16:12:24 +0530

    [Vanshika] : added CanStudentVote core java assignment

--- gcr-codebase/arrays/Level-1/CanStudentVote.java
+++ gcr-codebase/arrays/Level-1/CanStudentVote.java
@@ -1,6 +1,6 @@
-//Creating a class to take user input for the age of all 10 students in a class and check ingwhether the student can vote depending on his/her age is greater or equal to 18.
+//Creating a class to take user input for the age of all 10 students in a class and check ing whether the student can vote depending on his/her age is greater or equal to 18.
 
-import java.util.Arrays;
+
 import java.util.Scanner;
 public class CanStudentVote{
 	public static void main(String[] args){
@@ -9,22 +9,20 @@ public class CanStudentVote{
 		System.out.println("Ente age of 10 students : ");
 		//Storing the age values of 10 students through user input in array
 		for(int i=0; i<10; i++){
-			
 			students[i] = sc.nextInt();
 		}
-		
 		//Checking if student can vote or not
 		for(int i=0; i<students.length; i++){
 			if (students[i] < 0){
 				System.out.println("Invalid age");
 			} 
-
 			else if (students[i] >= 18){
 				System.out.println("The student with age " + students[i] +" can vote"); 		}
 			else {
 				System.out.println("The student with age " + students[i] +" cannot vote");
 			} 
 		}
+		sc.close();
 
 	}
 }
