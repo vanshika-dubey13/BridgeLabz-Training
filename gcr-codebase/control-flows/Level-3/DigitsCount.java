2a4e5b9806ef84c454971fb54f7af3d202a9475d
Vanshika Dubey <dubeyvanshika021@gmail.com>
2025-12-19 11:04:15 +0530

    [Vanshika] : added DigitsCount core java assignment

---
+++ gcr-codebase/control-flows/Level-3/DigitsCount.java
@@ -0,0 +1,28 @@
+gcr-codebase/control-flows/Level-3/DigitsInNumber.java
+//Creating a class to count number of digits in a number.
+
+import java.util.Scanner;
+
+public class DigitsCount {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        int num = sc.nextInt();
+
+        int count = 0;
+        if (num == 0) {
+            count = 1;
+        } else {
+            while (num != 0) {
+                 num = num / 10;
+                count++;
+            }
+        }
+
+        //  Displaying result
+        System.out.println("The number of digits is: " + count);
+
+        sc.close(); 
+    }
+}
+ 
