337e348dcb954f755d0a421bc109c114f4b9139b
Vanshika Dubey <dubeyvanshika021@gmail.com>
2026-01-08 13:30:46 +0530

    [Vanshika] : added Search2DMatrix solution

---
+++ dsa-practice/gcr-codebase/DsaConcepts/src/com/dsa/searching/binarysearching/Search2DMatrix.java
@@ -0,0 +1,49 @@
+package com.dsa.searching.binarysearching;
+
+public class Search2DMatrix {
+
+    public static boolean searchMatrix(int[][] matrix, int target) {
+        // Edge case is empty matrix
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return false;
+        }
+
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        int left = 0;
+        int right = rows * cols - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            int row = mid / cols;
+            int col = mid % cols;
+
+            int midValue = matrix[row][col];
+
+            if (midValue == target) {
+                return true;
+            } else if (midValue < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return false;
+    }
+
+    // main method
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 3, 5, 7},
+            {10, 11, 16, 20},
+            {23, 30, 34, 60}
+        };
+
+        int target = 16;
+        System.out.println(searchMatrix(matrix, target)); 
+    }
+}
+
