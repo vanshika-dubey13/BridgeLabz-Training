c883dc5e0f4920c0bbe7b738fb0c01c5c116b284
Vanshika Dubey <dubeyvanshika021@gmail.com>
2026-01-17 13:15:00 +0530

    [Vanshika] : added ZipZipMart solution

---
+++ dsa-practice/scenario-based/DsaScenarioBasedConcepts/src/dsa/scenariobased/sorting/ZipZipMart.java
@@ -0,0 +1,87 @@
+package dsa.scenariobased.sorting;
+
+import java.util.Scanner;
+
+//creating a class to sort sales report on amounts using merge sort
+public class ZipZipMart {
+
+    // Method to perform merge sort
+    public static void mergeSort(int[] prices, int left, int right) {
+        if (left < right) {
+            int mid = (left + right) / 2;
+            // Sorting left half
+            mergeSort(prices, left, mid);
+            // Sorting right half
+            mergeSort(prices, mid + 1, right);
+            // Merging the sorted halves
+            merge(prices, left, mid, right);
+        }
+    }
+
+    // Method to merge two sorted halves
+    public static void merge(int[] prices, int left, int mid, int right) {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        int[] leftArray = new int[n1];
+        int[] rightArray = new int[n2];
+
+        // Copying data to temporary arrays
+        for (int i = 0; i < n1; i++)
+            leftArray[i] = prices[left + i];
+        for (int j = 0; j < n2; j++)
+            rightArray[j] = prices[mid + 1 + j];
+        int i = 0, j = 0, k = left;
+
+        // Merging the arrays
+        while (i < n1 && j < n2) {
+            if (leftArray[i] <= rightArray[j]) {
+                prices[k] = leftArray[i];
+                i++;
+            } else {
+                prices[k] = rightArray[j];
+                j++;
+            }
+            k++;
+        }
+        // Copying remaining elements
+        while (i < n1) {
+            prices[k] = leftArray[i];
+            i++;
+            k++;
+        }
+        while (j < n2) {
+            prices[k] = rightArray[j];
+            j++;
+            k++;
+        }
+    }
+    
+  //method to display arr
+  	public static void printArr(int[] arr) {
+  		for(int i=0; i<arr.length; i++) {
+  			System.out.print(arr[i] +" ");
+  		}
+  	}
+  	
+    // Main method
+    public static void main(String[] args) {
+        
+    	Scanner sc = new Scanner(System.in);
+    	System.out.println("Enter number of sales done : ");
+    	int size =sc.nextInt();
+    	int[] salesAmount = new int[size];
+    	for(int i=0; i<size; i++) {
+    		System.out.println("Enter registration timr of "+(i+1)+" artist :");
+    		salesAmount[i]=sc.nextInt();
+    	}
+	
+    	//displaying results
+    	System.out.println("\nBefore sorting : ");
+    	printArr(salesAmount);
+    	System.out.println("\nAfter sorting : ");
+    	mergeSort(salesAmount, 0, salesAmount.length - 1);
+    	printArr(salesAmount);
+
+    }
+}
