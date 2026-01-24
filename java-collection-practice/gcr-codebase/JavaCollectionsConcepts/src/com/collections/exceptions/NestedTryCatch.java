060c275f12956211fb53eb9e8bfc60c4d13431f4
Vanshika Dubey <dubeyvanshika021@gmail.com>
2026-01-24 14:55:27 +0530

    [Vanshika] : added NestedTryCatch solution

---
+++ java-collection-practice/gcr-codebase/JavaCollectionsConcepts/src/com/collections/exceptions/NestedTryCatch.java
@@ -0,0 +1,38 @@
+package com.collections.exceptions;
+
+import java.util.Scanner;
+
+//creating a class to demonstrate usage of nested try catch
+public class NestedTryCatch {
+
+	//main method
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] nums = {10, 20, 30, 40, 50};
+        
+        try {
+            System.out.print("Enter array index to access: ");
+            int index = sc.nextInt();
+            
+            try {
+                System.out.print("Enter divisor: ");
+                int divisor = sc.nextInt();
+
+                int result = nums[index] / divisor;
+                System.out.println("Result: " + result);
+            }
+            
+            catch (ArithmeticException e) {
+                System.out.println("Cannot divide by zero!");
+            }
+
+        }
+        catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Invalid array index!");
+        }
+        
+        finally {
+            sc.close();
+        }
+    }
+}
